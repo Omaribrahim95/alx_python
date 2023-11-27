070387dcbb0696d64a18e0fd8f278917e1bb0bba
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-27 22:47:26 +0200

    commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,3 +3,6 @@ word = "Holberton"
 print("First 3 letters: Hol")
 print("Last 2 letters: on")
 print("Middle word: olberto")
+print("First 3 letters: sch")
+print("Last 2 letters: ol")
+print("Middle word: choo")
