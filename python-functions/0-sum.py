be61bd13795fd3eedce4a4d60e29967381bff67e
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-29 00:00:28 +0200

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,6 +1,13 @@
+import random
+
 def add(a, b):
     """Adds two integers and returns the result."""
     result = a + b
     return result
-sum = add(1, 2)
-print(sum)
+
+# Generate two random integers
+a = random.randint(1, 100)
+b = random.randint(1, 100)
+
+# Add the two integers and print the result
+print(add(a, b))
