8c905768621566dc010d66899d53ffdbdc8c99df
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-29 00:17:39 +0200

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,10 +1,2 @@
 def add(a, b):
-    """Adds two integers and returns the result."""
-    result = a + b
-    return result
-
-# Call the add function with two integers
-sum = add(1, 2)
-
-# Print the result
-print(sum)
+    return a + b
