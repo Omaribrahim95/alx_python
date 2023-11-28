e74427e2afb9bb7933ff4abf55699320905d7b05
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-28 23:43:54 +0200

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -12,8 +12,5 @@ def add(a, b):
   """
   return a + b
 
-add = __import__('0-sum').add
-# Test the function
-print(add(1, 2))  # Output: 3
-print(add(98, 0))  # Output: 98
-print(add(100, -2))  # Output: 98
+
+print(add(1, 2))
