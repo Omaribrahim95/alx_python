eba2493ff039acd6b848134ba72e4637ced30b99
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-28 22:07:17 +0200

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,3 @@
-for first_digit in range(10):
-  for second_digit in range(first_digit + 1, 10):
-    if first_digit * 10 + second_digit < second_digit * 10 + first_digit:
-      print("{0:02d}, ".format(first_digit * 10 + second_digit), end="")
-print()
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("%d%d, " % (i, j), end='')
