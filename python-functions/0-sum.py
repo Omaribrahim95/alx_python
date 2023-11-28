73aafa6b0264c3f833193114bfc77b9304fb7b28
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-28 23:55:31 +0200

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python3
 def add(a, b):
-  """
-  Adds two integers and returns the result.
-
-  Args:
-    a: The first integer to add.
-    b: The second integer to add.
-
-  Returns:
-    The sum of a and b.
-  """
-  return a + b
-print(add(1, 2))
+    """Adds two integers and returns the result."""
+    result = a + b
+    return result
+sum = add(1, 2)
+print(sum)
