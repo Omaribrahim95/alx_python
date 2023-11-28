d4460053ee720e5b3387c1c2302da2e4f13d9560
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-28 23:15:04 +0200

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,12 +1,7 @@
-flag = False
 for i in range(10):
   for j in range(10):
     if i < j:
       if i == 8 and j == 9:
-        flag = True
         print("{:01d}{:01d}".format(i, j))
       else:
         print("{:01d}{:01d}, ".format(i, j), end='')
-  if flag:
-    flag = False
-    print()
