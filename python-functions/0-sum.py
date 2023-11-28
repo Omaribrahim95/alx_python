c2567343ca8fe978de306920aad67c36c2b260e6
Omar Ibrahim <omaribrahim119955@gmail.com>
2023-11-29 00:11:53 +0200

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,13 +1,10 @@
-import time
-
 def add(a, b):
     """Adds two integers and returns the result."""
     result = a + b
     return result
 
-# Generate two random integers using time.time() as a seed
-a = int(time.time() * 1000) % 100 + 1
-b = int(time.time() * 1000) % 100 + 1
+# Call the add function with two integers
+sum = add(1, 2)
 
-# Add the two integers and print the result
-print(add(a, b))
+# Print the result
+print(sum)
